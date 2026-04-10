0e0a6a13bd8ad9987bcd66b14f1e924ce224efb0
kiv1n <aloha@kiv1n.ru>
2026-04-10 17:07:27 +0300

    Refactor IndexPage layout: implement new sections for hero, technologies, activity, and agents; add separator component and skeleton placeholders

---
+++ components/index/index-activity-section.tsx
@@ -0,0 +1,16 @@
+import { cn } from "@/lib/utils";
+import { ClassValue } from "clsx";
+import { Skeleton } from "../ui/skeleton";
+
+// TODO: Implement this section
+export function IndexActivitySection(props: { className?: ClassValue }) {
+  return (
+    <section className={cn(props.className)}>
+      <div className="flex flex-col gap-2">
+        <Skeleton className="h-8" />
+        <Skeleton className="h-8" />
+        <Skeleton className="h-8" />
+      </div>
+    </section>
+  );
+}
