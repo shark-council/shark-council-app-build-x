3015df9e1293a19bed2ab380461122bde659674a
kiv1n <aloha@kiv1n.ru>
2026-04-14 14:49:48 +0300

    Implement activity section with metrics display and create Item component for UI

--- components/index/index-activity-section.tsx
+++ components/index/index-activity-section.tsx
@@ -1,15 +1,70 @@
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Item, ItemContent, ItemMedia, ItemTitle } from "@/components/ui/item";
 import { cn } from "@/lib/utils";
 import { ClassValue } from "clsx";
-import { Skeleton } from "../ui/skeleton";
+import {
+  BotIcon,
+  MessagesSquareIcon,
+  RefreshCwIcon,
+  UsersIcon,
+} from "lucide-react";
 
-// TODO: Implement this section
 export function IndexActivitySection(props: { className?: ClassValue }) {
   return (
     <section className={cn(props.className)}>
-      <div className="flex flex-col gap-2">
-        <Skeleton className="h-8" />
-        <Skeleton className="h-8" />
-        <Skeleton className="h-8" />
+      <p className="font-bold text-center">Activity</p>
+      <p className="text-muted-foreground text-center">
+        Metrics from the ecosystem
+      </p>
+      <div className="grid grid-cols-2 gap-2 mt-4">
+        <Item variant="outline">
+          <ItemMedia variant="icon">
+            <Avatar className="size-12">
+              <AvatarFallback className="bg-primary text-primary-foreground">
+                <BotIcon />
+              </AvatarFallback>
+            </Avatar>
+          </ItemMedia>
+          <ItemContent>
+            <ItemTitle>4 Sharks</ItemTitle>
+          </ItemContent>
+        </Item>
+        <Item variant="outline">
+          <ItemMedia variant="icon">
+            <Avatar className="size-12">
+              <AvatarFallback className="bg-accent text-accent-foreground">
+                <MessagesSquareIcon />
+              </AvatarFallback>
+            </Avatar>
+          </ItemMedia>
+          <ItemContent>
+            <ItemTitle>23 Councils</ItemTitle>
+          </ItemContent>
+        </Item>
+        <Item variant="outline">
+          <ItemMedia variant="icon">
+            <Avatar className="size-12">
+              <AvatarFallback className="bg-accent text-accent-foreground">
+                <RefreshCwIcon />
+              </AvatarFallback>
+            </Avatar>
+          </ItemMedia>
+          <ItemContent>
+            <ItemTitle>11 Trades</ItemTitle>
+          </ItemContent>
+        </Item>
+        <Item variant="outline">
+          <ItemMedia variant="icon">
+            <Avatar className="size-12">
+              <AvatarFallback className="bg-primary text-primary-foreground">
+                <UsersIcon />
+              </AvatarFallback>
+            </Avatar>
+          </ItemMedia>
+          <ItemContent>
+            <ItemTitle>5 Users</ItemTitle>
+          </ItemContent>
+        </Item>
       </div>
     </section>
   );
